fbf0ebcad775122ca9d24840283a13e6763c4431
Mario Araya Romero <arayaromero@gmail.com>
2017-07-11 22:20:34 -0400

    Home, Search, Navbar components generados con ng.cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
+    APP_ROUTING,
     BrowserModule
   ],
   providers: [],
