84e711be0efdb434de818f6f7578bb9a30149c60
Mario Araya Romero <arayaromero@gmail.com>
2017-07-14 11:44:11 -0400

    metodo getArtista() y getTopSongs()

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,13 +6,14 @@ import 'rxjs/add/operator/map'
 export class SpotifyService {
   public artistas: any = []
   public urlBusqueda: string = "https://api.spotify.com/v1/search"
+  public urlArtista : string = "https://api.spotify.com/v1/artists"
 
 
   constructor(private http:Http) { }
 
   getArtistas(termino: string) {
     let headers = new Headers()
-    headers.append("authorization", "Bearer BQAHzUOolTY6bz5FS2KBhZYDaMdFS-MrzqMouPjO1ppTHafsUrr1SljbRRjAgQogjXSlG8yIb4Zr6ZrpKLw_OA")
+    headers.append("authorization", "Bearer BQA1qRzv5Vokcs6wn2P4kqg7eXA77y9ijX7jUPskN97792eZBY9zcyXo3uzwpRyl3nJsUwzP5PyQP9Zn0mqhHw")
     let query = `?q=${ termino }&type=artist`;
     let url = this.urlBusqueda + query;
 
@@ -23,6 +24,32 @@ export class SpotifyService {
             console.log( this.artistas )
 
             return this.artistas;
+          })
+  }
+
+  getArtista(id: string) {
+    let headers = new Headers()
+    headers.append("authorization", "Bearer BQBOEGTnadY8Dd8Zd1_k6_eeSAIh4CtUhfPD5yLMov-0GvmozGPmX2wgFeZjrwMq7cV0nprnaoiYyJbh0BYpjA")
+    let query = `/${ id }`;
+    let url = this.urlArtista + query;
+
+    return this.http.get( url, { headers } ) //retorna observable
+          .map( res => {
+            console.log( res.json() )
+            return res.json()
+          })
+  }
+
+  getTopSongs(id: string) {
+    let headers = new Headers()
+    headers.append("authorization", "Bearer BQBOEGTnadY8Dd8Zd1_k6_eeSAIh4CtUhfPD5yLMov-0GvmozGPmX2wgFeZjrwMq7cV0nprnaoiYyJbh0BYpjA")
+    let query = `/${ id }/top-tracks?country=US`;
+    let url = this.urlArtista + query;
+
+    return this.http.get( url, { headers } ) //retorna observable
+          .map( res => {
+            console.log( res.json() )
+            return res.json().tracks
           })
   }
 }
